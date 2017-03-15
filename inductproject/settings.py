1e2201b466b7990d6b68a75f6903c4cac502cfcc
Shubham Gore <shubham.gore@gslab.com>
2017-03-15 21:52:23 +0530

    commit-1

--- inductproject/settings.py
+++ inductproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'snapp.apps.SnappConfig'
 ]
 
 MIDDLEWARE = [
--- snapp/models.py
+++ snapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import requests
 
-# Create your models here.
+# Set the request parameters
+url = 'https://dev20632.service-now.com/api/now/table/incident'
+user = 'admin'
+pwd = 'Qazwsx@123'
+
+# Set proper headers
+headers = {"Accept": "application/json"}
+
+# Do the HTTP request
+response = requests.get(url, auth=(user, pwd), headers=headers)
+
+# Check for HTTP codes other than 200
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+print('Status:', response.status_code, 'Headers:', response.headers, 'Response:', response.json())
+print('Cookies', response.cookies)
