c1c292c573cbeef686647e07eabca777eb337197
Shubham Gore <shubham.gore@gslab.com>
2017-03-17 02:01:29 +0530

    List and Details are done

--- snapp/urls.py
+++ snapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 # We are adding a URL called /home
 urlpatterns = [
     url(r'^$', views.logi, name='home'),
-    url(r'^list/$', views.listall, name='listv'),
+    url(r'^list/$', views.listall, name='list'),
+    url(r'^incident_details/(?P<sys_id>[A-Za-z0-9_@./&+-]+)$', views.incidentdet, name='incident_details'),
 ]
--- snapp/views.py
+++ snapp/views.py
@@ -9,6 +9,12 @@ def logi(request):
     return render(request,"home.html")
 
 ##################################################################################################################################################
+# Connection details
+class ConnDet:
+    url = 'https://dev20632.service-now.com/api/now/table/incident'
+    user = 'admin'
+    pwd = 'Qazwsx@123'
+
 # Get Connection
 def getConnectSnList():
     # Set the request parameters
@@ -48,3 +54,38 @@ def getConnectSnList():
 def listall(request):
     dictOfList = getConnectSnList()
     return render(request, "list.html", { 'instancelist': dictOfList })
+
+#######################################################################################################################
+def getConnectDetail(sys_id):
+    url = ConnDet.url + '/' + sys_id
+    user = ConnDet.user
+    pwd = ConnDet.pwd
+
+    # Set proper headers
+    headers = {"Accept": "application/json"}
+
+    # Do the HTTP request
+    response = requests.get(url, auth=(user, pwd), headers=headers)
+
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+        exit()
+
+    # Decode the JSON response into a dictionary and use the data
+    if response.status_code == 200:
+        print('Status:', response.status_code)
+        #print('*************')
+
+    resData = json.dumps(response.json())
+    resDataJson = json.loads(resData)
+
+    parseRes = resDataJson['result']
+    print('#################')
+    #print(resDataJson)
+    return parseRes
+
+@login_required(login_url="login/")
+def incidentdet(request,sys_id):
+    dictOfDetails = getConnectDetail(sys_id)
+    return render(request, 'incidet.html',{ 'instancedetaildict': dictOfDetails })
