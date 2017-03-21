4e3e166cb034dcf33a0e245408e21e90fc0652eb
Shubham Gore <shubham.gore@gslab.com>
2017-03-21 19:51:38 +0530

    Commit after model implemenation which is used to configure service now instanace

--- snapp/admin.py
+++ snapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from  snapp.models import configServiceNow
 
 # Register your models here.
+class ConfigServiceAdmin(admin.ModelAdmin):
+    list_display = ["host","username","enable"]
+    search_fields = ["host" , "username"]
+
+    class Meta:
+        model = configServiceNow
+
+admin.site.register(configServiceNow , ConfigServiceAdmin)
--- snapp/models.py
+++ snapp/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class configServiceNow(models.Model):
+    host = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    enable = models.BooleanField(default= False)
+
+    def __str__(self):
+        return self.host
